493079c07e65eaa69629f9360a5df285041e104f
pengin <92212932+PenginPenPen@users.noreply.github.com>
2024-08-18 20:14:34 +0900

    ページ名の変更

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
+        let viewController = DebugPageViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
