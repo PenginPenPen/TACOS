67790998295a3679020e56fbd7be5e6f61350d03
pengin <92212932+PenginPenPen@users.noreply.github.com>
2024-08-20 13:11:54 +0900

    データベースからデータを取得するコードを追加

--- TimelineViewController.swift
+++ TimelineViewController.swift
@@ -6,17 +6,39 @@
 //
 
 import UIKit
-
+import Firebase
+import FirebaseFirestore
 class TimelineViewController: UIViewController {
-
-    override func viewDidLoad() {
+    var db = Firestore.firestore()
+    override func viewDidLoad(){
         super.viewDidLoad()
         setupView()
+        Task {
+            do {
+                let _: () =  await getData()
+            }
+        }
         // Do any additional setup after loading the view.
     }
     private func setupView(){
         self.title="タイムライン"
         view.backgroundColor =  UIColor(red: 0.98, green: 1.00, blue: 0.25, alpha: 1.0)
+    }
+    private func getData() async{
+        let docRef = db.collection("TestData").document("TestPost")
+        
+        do {
+          let document = try await docRef.getDocument()
+          if document.exists {
+            let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+            print("Document data: \(dataDescription)")
+          } else {
+            print("Document does not exist")
+          }
+        } catch {
+          print("Error getting document: \(error)")
+        }
+    }
     }
     
 
@@ -29,8 +51,6 @@ class TimelineViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
 #Preview(){
     TimelineViewController()
 }
