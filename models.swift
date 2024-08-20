8d4c7e737cb552a00935910d20f4777550b6b107
pengin <92212932+PenginPenPen@users.noreply.github.com>
2024-08-20 13:12:42 +0900

    modelsファイルの追加

---
+++ models.swift
@@ -0,0 +1,14 @@
+//
+//  models.swift
+//  TACOS
+//
+//  Created by 疋田朋也 on 2024/08/18.
+//
+
+import Foundation
+struct Post {
+    let userId: String
+    let username: String
+    let text: String
+    let timestamp: Date
+}
