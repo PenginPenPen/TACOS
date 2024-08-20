15b7cc2348f34554c93eeea1695e014a593bddc3
pengin <92212932+PenginPenPen@users.noreply.github.com>
2024-08-20 13:12:11 +0900

    投稿ページの追加

---
+++ AddPostViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddPostViewController.swift
+//  TACOS
+//
+//  Created by 疋田朋也 on 2024/08/18.
+//
+
+import UIKit
+
+class AddPostViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+#Preview(){
+    AddPostViewController()
+}
