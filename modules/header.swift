1daab88711d0128e4aefcbcfd68104e7a4c15b2a
pengin <92212932+PenginPenPen@users.noreply.github.com>
2024-08-27 23:48:02 +0900

    ヘッダーのアイコンからプロフィールに移行する機能の追加

--- modules/header.swift
+++ modules/header.swift
@@ -8,9 +8,14 @@
 import Foundation
 import UIKit
 
-extension UIViewController {
+protocol CustomHeaderViewDelegate: AnyObject {
+    func headerViewDidTapButton(_ headerView: CustomHeaderView)
+}
+
+extension UIViewController: CustomHeaderViewDelegate {
     func setupCustomHeader() {
-        let headerView = CustomHeaderView(frame: .zero)
+        let headerView = CustomHeaderView()
+        headerView.delegate = self
         headerView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(headerView)
         
@@ -18,12 +23,19 @@ extension UIViewController {
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 60) // ヘッダーの高さを適切に調整
+            headerView.heightAnchor.constraint(equalToConstant: 60)
         ])
+    }
+    
+    func headerViewDidTapButton(_ headerView: CustomHeaderView) {
+        let  UserProfile = UserProfileViewController()
+        self.navigationController?.pushViewController( UserProfile, animated: true)
     }
 }
 
 class CustomHeaderView: UIView {
+    weak var delegate: CustomHeaderViewDelegate?
+    
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "ヘッダーだよ"
@@ -34,11 +46,12 @@ class CustomHeaderView: UIView {
         return label
     }()
     
-    private let iconImageView: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "defaultUserIcon"))
-        imageView.contentMode = .scaleAspectFit
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
+    private let iconButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "sample"), for: .normal)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override init(frame: CGRect) {
@@ -54,16 +67,21 @@ class CustomHeaderView: UIView {
     private func setupView() {
         self.backgroundColor = UIColor(red: 0.16, green: 0.16, blue: 0.16, alpha: 1.0)
         addSubview(titleLabel)
-        addSubview(iconImageView)
+        iconButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        addSubview(iconButton)
         
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -40),
             titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-
-            iconImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
-            iconImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            iconImageView.widthAnchor.constraint(equalToConstant: 32),
-            iconImageView.heightAnchor.constraint(equalToConstant: 32)
+            
+            iconButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            iconButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            iconButton.widthAnchor.constraint(equalToConstant: 32),
+            iconButton.heightAnchor.constraint(equalToConstant: 32)
         ])
+    }
+    
+    @objc private func buttonTapped() {
+        delegate?.headerViewDidTapButton(self)
     }
 }
